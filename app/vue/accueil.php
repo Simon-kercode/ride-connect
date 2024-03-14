ddf6b2ca5739fdc6db128bd58faf6371e35eff32
Simon-kercode <sim.malry@gmail.com>
2024-03-14 11:18:47 +0100

    mise en place sass

---
+++ app/vue/accueil.php
@@ -0,0 +1,8 @@
+<section>
+    <div id="welcomeSearch">
+        <!-- <img id="welcomePic" src="public/images/photo-accueil-sombre.png" alt="motards alignés"> -->
+        <h1>Rouler,<br> emsemble</h1>
+        <label for="search"></label>
+        <input id="welcomeSearchField" type="search" name="search">
+    </div>
+</section>
